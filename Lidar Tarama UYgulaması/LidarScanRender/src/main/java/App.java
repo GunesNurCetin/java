6152dbee7740d360f5e4abe14931e2847a935c82
Güneş Nur Çetin <160675617+GunesNurCetin@users.noreply.github.com>
2025-05-23 14:50:48 +0300

    Add files via upload
    Lidar ile nesne tarama Uygulaması

---
+++ Lidar Tarama UYgulaması/LidarScanRender/src/main/java/App.java
@@ -0,0 +1,8 @@
+public class App {
+    //JavaFX Gradle projects need to run from a class that does not extend Application in order for the Run button to work.
+
+    //Run this to start the app
+    public static void main(String[] args) {
+        MainLogic.main(args);
+    }
+}
